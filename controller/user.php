5358a1110a4e4c7dc496bf99c7cc1d41dcc744dd
fmoreiranet <fabianomoreira.net@gmail.com>
2019-01-23 09:32:05 -0200

    formulario

--- controller/user.php
+++ controller/user.php
@@ -1,7 +1,9 @@
 <?php
-    class User{
-        private $nome;
-        private $email;
-        private $pws;
+   $user = $_POST;
 
-    }
+   $nome = $_POST['nome'];
+
+   var_dump($user);
+
+   echo "<h3> $user[nome] </h3>";
+   echo "<h3> $nome </h3>";
--- view/cadastro.php
+++ view/cadastro.php
@@ -1,3 +1,15 @@
 <section class="container">
     <H2>Cadastro de User</H2>
+    <form action="./controller/user.php" method="POST">
+        <label> Nome </label>
+        <input type="text" name="nome">
+
+        <label> E-mail </label>
+        <input type="email" name="email">
+
+        <label> Senha </label>
+        <input type="password" name="pws">
+
+        <button>Salvar</button>
+    </form>
 </section>
