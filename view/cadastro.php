e2c0635d1807a4db3fc74acc198ceaf8734f3788
fmoreiranet <fabianomoreira.net@gmail.com>
2019-01-23 09:56:56 -0200

    formulario

--- view/cadastro.php
+++ view/cadastro.php
@@ -2,14 +2,14 @@
     <H2>Cadastro de User</H2>
     <form action="./controller/user.php" method="POST">
         <label> Nome </label>
-        <input type="text" name="nome">
+        <input type="text" name="nome" class="form-control">
 
         <label> E-mail </label>
-        <input type="email" name="email">
+        <input type="email" name="email" class="form-control">
 
         <label> Senha </label>
-        <input type="password" name="pws">
+        <input type="password" name="pws" class="form-control">
 
-        <button>Salvar</button>
+        <button  class="btn btn-primary">Salvar</button>
     </form>
 </section>
